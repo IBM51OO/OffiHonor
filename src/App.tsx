cd35461a1cae9e76979864a761c75b071d0e9e06
Atsamaz Tuaev <4artor@gmail.com>
2022-09-21 18:40:28 +0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import classes from './App.module.css';
+import resets from './components/_resets.module.css';
+import { Frame1280 } from './components/Frame1280/Frame1280';
+
+interface Props {
+  className?: string;
+}
+export const App: FC<Props> = memo(function App(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <Frame1280 />
+    </div>
+  );
+});
---
+++ src/components/Frame1280/Frame1280.tsx
@@ -0,0 +1,272 @@
+import { memo } from 'react';
+import type { FC } from 'react';
+
+import resets from '../_resets.module.css';
+import classes from './Frame1280.module.css';
+import { ImLogo1Icon } from './ImLogo1Icon';
+
+interface Props {
+  className?: string;
+}
+/* @figmaId 1:2 */
+export const Frame1280: FC<Props> = memo(function Frame1280(props = {}) {
+  return (
+    <div className={`${resets.clapyResets} ${classes.root}`}>
+      <div className={classes.rectangle25}></div>
+      <div className={classes.unnamed}>
+        <p className={classes.labelWrapper}>
+          <span className={classes.label}>О Фонде Проекты Чем помочь Отчеты Партнеры Новости Контакты </span>
+          <span className={classes.label2}> </span>
+          <span className={classes.label3}>мне нужна помощь</span>
+        </p>
+        <div className={classes.textBlock}>
+          <p></p>
+        </div>
+      </div>
+      <div className={classes._7000000}>+ 7 (000) 000-00-00</div>
+      <div className={classes.rectangle1}></div>
+      <div className={classes.unnamed2}>Я хочу помочь</div>
+      <div className={classes.unnamed3}>Войти</div>
+      <div className={classes._77e7709c19574959caa213f7f1229f}></div>
+      <div className={classes.rectangle19}></div>
+      <div className={classes.unnamed4}>Подписаться на почтовую рассылку</div>
+      <div className={classes.rectangle20}></div>
+      <div className={classes.eMail}>E-mail</div>
+      <div className={classes.EMail}>Введите E-mail</div>
+      <div className={classes.rectangle252}></div>
+      <div className={classes.unnamed5}>Имя</div>
+      <div className={classes.rectangle26}></div>
+      <div className={classes.unnamed6}>Подписаться</div>
+      <div className={classes.unnamed7}>Ваше имя</div>
+      <div className={classes.unnamed8}>
+        <div className={classes.textBlock2}>О Фонде</div>
+        <div className={classes.textBlock3}>Проекты</div>
+        <div className={classes.textBlock4}>Чем помочь</div>
+        <div className={classes.textBlock5}>Отчеты</div>
+        <div className={classes.textBlock6}>Партнеры</div>
+        <div className={classes.textBlock7}>Новости и мероприятия</div>
+        <div className={classes.textBlock8}>Контакты</div>
+        <div className={classes.textBlock9}>
+          <p></p>
+        </div>
+      </div>
+      <div className={classes._70000002}>+ 7 (000) 000-00-00</div>
+      <div className={classes.rectangle23}></div>
+      <div className={classes.unnamed9}>Я хочу помочь</div>
+      <div className={classes.rectangle24}></div>
+      <div className={classes.unnamed10}>Мне нужна помощь</div>
+      <div className={classes._2022Copyright}>2022.Copyright</div>
+      <div className={classes._02LOGO1}></div>
+      <div className={classes.magnifyingGlass2}></div>
+      <div className={classes.unnamed11}>Адресная помощь военным</div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.
+      </div>
+      <div className={classes.old1701640_19201}></div>
+      <div className={classes._3805_peregivanie_gory31}></div>
+      <div className={classes.rusuKaroPolicininkasSirijoje83}></div>
+      <div className={classes.unnamed12}>Наши проекты</div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte2}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam
+      </div>
+      <div className={classes.line1}></div>
+      <div className={classes.line2}></div>
+      <div className={classes.line3}></div>
+      <div className={classes.line4}></div>
+      <div className={classes.rectangle262}></div>
+      <div className={classes.loremIpsumDolor}>Lorem ipsum dolor</div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte3}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.{' '}
+      </div>
+      <div className={classes.rectangle27}></div>
+      <div className={classes.loremIpsumDolor2}>Lorem ipsum dolor</div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte4}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.{' '}
+      </div>
+      <div className={classes.rectangle28}></div>
+      <div className={classes.loremIpsumDolor3}>Lorem ipsum dolor</div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte5}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.{' '}
+      </div>
+      <div className={classes.unnamed13}>Посмотреть все</div>
+      <div className={classes.image1}></div>
+      <div className={classes.image12}></div>
+      <div className={classes.image13}></div>
+      <div className={classes.image14}></div>
+      <div className={classes.image15}></div>
+      <div className={classes.unnamed14}>Наши партнеры</div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte6}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam
+      </div>
+      <div className={classes.line12}></div>
+      <div className={classes.unnamed15}>О Фонде</div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte7}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.{' '}
+      </div>
+      <div className={classes.unnamed16}>Кто мы?</div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte8}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum
+      </div>
+      <div className={classes.unnamed17}>Что мы делаем?</div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte9}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        laborum.
+      </div>
+      <div className={classes._1016076362_0_222_2854_1827_600}></div>
+      <div className={classes.rectangle29}></div>
+      <div className={classes.rectangle30}></div>
+      <div className={classes.rectangle31}></div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte10}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </div>
+      <div className={classes.unnamed18}>Сбор денежных средств</div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte11}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat{' '}
+      </div>
+      <div className={classes._23456713234}>
+        <p className={classes.labelWrapper2}>
+          <span className={classes.label4}>Необходимая сумма </span>
+          <span className={classes.label5}>234 567 руб.</span>
+        </p>
+        <div className={classes.textBlock10}>
+          <p className={classes.labelWrapper3}>
+            <span className={classes.label6}>Не хватает </span>
+            <span className={classes.label7}>13 234 руб.</span>
+          </p>
+        </div>
+      </div>
+      <div className={classes.rectangle12}></div>
+      <div className={classes.unnamed19}>Я хочу помочь</div>
+      <div className={classes._624d352655b991}></div>
+      <div className={classes.rectangle292}></div>
+      <div className={classes.rectangle302}></div>
+      <div className={classes.rectangle312}></div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte12}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </div>
+      <div className={classes.unnamed20}>Сбор денежных средств</div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte13}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat{' '}
+      </div>
+      <div className={classes._234567132342}>
+        <p className={classes.labelWrapper4}>
+          <span className={classes.label8}>Необходимая сумма </span>
+          <span className={classes.label9}>234 567 руб.</span>
+        </p>
+        <div className={classes.textBlock11}>
+          <p className={classes.labelWrapper5}>
+            <span className={classes.label10}>Не хватает </span>
+            <span className={classes.label11}>13 234 руб.</span>
+          </p>
+        </div>
+      </div>
+      <div className={classes.rectangle13}></div>
+      <div className={classes.unnamed21}>Я хочу помочь</div>
+      <div className={classes.line42}></div>
+      <div className={classes.unnamed22}>Посмотреть все</div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte14}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam
+      </div>
+      <div className={classes.unnamed23}>Сейчас нуждаются в помощи</div>
+      <div className={classes.rectangle32}></div>
+      <div className={classes.unnamed24}>Как помочь?</div>
+      <div className={classes.unnamed25}>
+        <div className={classes.textBlock12}>Варианты помощи:</div>
+        <div className={classes.textBlock13}>
+          <p className={classes.labelWrapper6}></p>
+        </div>
+        <div className={classes.textBlock14}>
+          <p></p>
+        </div>
+      </div>
+      <div className={classes.unnamed26}>Перевод денежных средств</div>
+      <div className={classes.rectangle33}></div>
+      <div className={classes.unnamed27}>Благотворитель</div>
+      <div className={classes.unnamed28}>Анонимно</div>
+      <div className={classes.rectangle43}></div>
+      <div className={classes.unnamed29}>Назначение платежа</div>
+      <div className={classes.unnamed30}>Пожертвование</div>
+      <div className={classes.rectangle40}></div>
+      <div className={classes.eMail2}>E-mail для квитанции</div>
+      <div className={classes.eMail3}>E-mail</div>
+      <div className={classes.unnamed31}>Сумма пожертвования</div>
+      <div className={classes.rectangle34}></div>
+      <div className={classes._100}>100 руб.</div>
+      <div className={classes.rectangle35}></div>
+      <div className={classes.rectangle37}></div>
+      <div className={classes.rectangle38}></div>
+      <div className={classes._500}>500 руб.</div>
+      <div className={classes.rectangle36}></div>
+      <div className={classes._1000}>1000 руб.</div>
+      <div className={classes.unnamed32}>ввести другую сумму</div>
+      <div className={classes.unnamed33}>единоразовое пожертвование</div>
+      <div className={classes.unnamed34}>ежемесячное пожертвование</div>
+      <div className={classes.rectangle41}></div>
+      <div className={classes.rectangle42}></div>
+      <div className={classes.unnamed35}>
+        <p className={classes.labelWrapper7}>
+          <span className={classes.label12}>Я принимаю </span>
+          <span className={classes.label13}>условия публичной оферты</span>
+        </p>
+      </div>
+      <div className={classes.unnamed36}>
+        <p className={classes.labelWrapper8}>
+          <span className={classes.label14}>Я принимаю </span>
+          <span className={classes.label15}>условия обработки персональных данных</span>
+        </p>
+      </div>
+      <div className={classes.rectangle14}></div>
+      <div className={classes.unnamed37}>ОТправить</div>
+      <div className={classes.comment1}></div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte15}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur.
+      </div>
+      <div className={classes.comment2}></div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte16}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur.
+      </div>
+      <div className={classes.comment3}></div>
+      <div className={classes.loremIpsumDolorSitAmetConsecte17}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur.
+      </div>
+      <div className={classes.unnamed38}>разработано в</div>
+      <div className={classes.iMLogo1}>
+        <ImLogo1Icon className={classes.icon} />
+      </div>
+    </div>
+  );
+});
